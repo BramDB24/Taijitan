1e70f92b1cc7a7ad470c22f34eacbd3f586929a5
Jonah De Smet <jonah.desmet@student.hogent.be>
2019-02-15 12:54:06 +0100

    Add project files.

---
+++ G07_Taijitan/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace G07_Taijitan.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
