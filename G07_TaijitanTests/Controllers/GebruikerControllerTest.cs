36659ba6678d000a2c4e335574a9d6fdc90dcde1
Jonah De Smet <jonah.desmet@hotmail.com>
2019-02-20 23:11:15 +0100

    Aanmaken GebruikerControllerTest

---
+++ G07_TaijitanTests/Controllers/GebruikerControllerTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using G07_Taijitan.Controllers;
+using G07_Taijitan.Models.Domain;
+using Moq;
+using Xunit;
+
+namespace G07_Taijitan.Tests.Controllers
+{
+    public class GebruikerControllerTest
+    {
+        private readonly GebruikerController _controller;
+        private readonly Mock<IGebruikerRepository> _gebruikersRepository;
+
+
+        public GebruikerControllerTest()
+        {
+            _gebruikersRepository = new Mock<IGebruikerRepository>();
+        }
+
+        #region Index
+        [Fact]
+        public void Index_GebruikersAantalIsNotNull()
+        {
+            _gebruikersRepository.Setup(v => v.GetGetAllGebruikers()).Returns();
+        }
+        #endregion
+    }
+}
