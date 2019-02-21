dfe2d2bbafb1b2042e8515869ace6ac348e8d0dd
Jonah De Smet <jonah.desmet@hotmail.com>
2019-02-21 12:37:38 +0100

    Add gebruikerControllerTest
    -- reference problems

--- G07_TaijitanTests/Controllers/GebruikerControllerTest.cs
+++ G07_TaijitanTests/Controllers/GebruikerControllerTest.cs
@@ -3,27 +3,36 @@ using System.Collections.Generic;
 using System.Text;
 using G07_Taijitan.Controllers;
 using G07_Taijitan.Models.Domain;
+using G07_Taijitan.Tests.Data;
 using Moq;
 using Xunit;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 
 namespace G07_Taijitan.Tests.Controllers
 {
     public class GebruikerControllerTest
     {
         private readonly GebruikerController _controller;
+        private readonly DummyApplicationDbContext _dummyContext;
         private readonly Mock<IGebruikerRepository> _gebruikersRepository;
 
 
         public GebruikerControllerTest()
         {
+            _dummyContext = new DummyApplicationDbContext();
             _gebruikersRepository = new Mock<IGebruikerRepository>();
         }
 
         #region Index
         [Fact]
-        public void Index_GebruikersAantalIsNotNull()
+        public void Index_GebruikersAanwezig_GeeftGeordendeLijstVanGebruikersDoorViaViewModel()
         {
-            _gebruikersRepository.Setup(v => v.GetGetAllGebruikers()).Returns();
+            _gebruikersRepository.Setup(v => v.GetGetAllGebruikers()).Returns(_dummyContext.Gebruikers);
+            ViewResult actionresult = _controller.Index(null) as ViewResult;
+            var gebruikers = actionresult?.Model as IEnumerable<Gebruiker>;
+            Assert.Equal(2, gebruikers.Count());
         }
         #endregion
     }
