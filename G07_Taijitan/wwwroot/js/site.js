73cf1166cbff73238551eaca7296e0e2cecea7a1
Lowie Scheirlinckx <lowie.scheirlinckx@student.hogent.be>
2019-03-21 22:22:07 +0100

    Lijst aanwezigheden javascript

--- G07_Taijitan/wwwroot/js/site.js
+++ G07_Taijitan/wwwroot/js/site.js
@@ -3,19 +3,26 @@
 
 // Write your JavaScript code. 
 var aanwezigeLeden = new Array();
+var afwezigeLeden = new Array();
+    Array.from(document.getElementsByClassName("leden")).forEach(function (element) {
+    afwezigeLeden.push(element.id);
+});
+
+
     function registreerAanwezigheid(id)
     {
         if (aanwezigeLeden.includes(id)) {
+            afwezigeLeden.push(id);
             var index = aanwezigeLeden.indexOf(id);
             if (index !== -1) aanwezigeLeden.splice(index, 1);
             document.getElementById(id).style.backgroundColor = "#FFFFFF"; // backcolor
 
         } else {
-            document.getElementById(id).style.backgroundColor = "#7FFF00"; // backcolor
             aanwezigeLeden.push(id);
-        }
-        console.log(aanwezigeLeden);
+            if (index !== -1) afwezigeLeden.splice(index, 1);
+            document.getElementById(id).style.backgroundColor = "#7FFF00"; // backcolor
             
+        } 
         return false;
     }
 
