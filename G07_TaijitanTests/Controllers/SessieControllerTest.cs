28d23ce6e523c7bee1902325d222557edf13a8c6
Johanna De Bruycker <johanna.debruycker@student.hogent.be>
2019-03-27 14:10:14 +0100

    Test SessieController

---
+++ G07_TaijitanTests/Controllers/SessieControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using G07_Taijitan.Controllers;
+using G07_Taijitan.Models.Domain.RepoInterface;
+using G07_Taijitan.Tests.Data;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace G07_Taijitan.Tests.Controllers
+{
+    class SessieControllerTest
+    {
+        private readonly SessieController _controller;
+        private readonly DummyApplicationDbContext _dummyContext;
+        private readonly Mock<ISessieRepository> _sessieRepository;
+
+        public SessieControllerTest()
+        {
+            _dummyContext = new DummyApplicationDbContext();
+            _sessieRepository = new Mock<ISessieRepository>();
+
+        }
+    }
+}
